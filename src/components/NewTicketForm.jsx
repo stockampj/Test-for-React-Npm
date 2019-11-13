8b846966945b0982ed76d1852cc352f805892f32
Student <example@example.com>
2019-11-13 09:34:11 -0800

    add stuff

---
+++ src/components/NewTicketForm.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function NewTicketForm() {
+    return (
+        <div>
+            <form>
+                <input
+                    type='text'
+                    id='names'
+                    placeholder='Pair Names'
+                />
+                <input
+                    type='text'
+                    id='location'
+                    placeholder='Location'
+                />
+                <textarea
+                    id='issue'
+                    placeholder='Describe your issue:'
+                />
+                <button type='submit'>Halp!</button>
+            </form>
+        </div>
+    );
+}
+
+export default NewTicketForm;
